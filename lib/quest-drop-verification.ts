7ccfc7924f3ea3f83fcc88f15c1b9a73aaad5310
sukanto01899 <sukanto01899@gmail.com>
2026-03-12 12:00:35 +0600

    update

---
+++ lib/quest-drop-verification.ts
@@ -0,0 +1,14 @@
+export type QuestVerificationRule = {
+  requireVisit: boolean;
+  requireFree: boolean;
+  requireEmber: boolean;
+  requireCelestial: boolean;
+};
+
+// Edit these booleans to control quest-drop verification globally.
+export const questDropVerificationRule: QuestVerificationRule = {
+  requireVisit: true,
+  requireFree: false,
+  requireEmber: false,
+  requireCelestial: false,
+};
