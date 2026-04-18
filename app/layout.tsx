bd24d9c9d4404fadde60a40d556ae4737851de57
sukanto01899 <sukanto01899@gmail.com>
2026-04-19 01:56:10 +0600

    talent verification added

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,10 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Base Farcaster MiniApp Template",
   description: "A template for building mini-apps on Farcaster and Base",
+  other: {
+    "talentapp:project_verification":
+      "5d324d1817f3897d25b0b84dfa7ee6a7bdc67aeb3008b204857c826fc8e71f2302d49e1ed825190eeba978852b760c8580d60415f5695546e20f6e78e37118dc",
+  },
 };
 const config = {
   rpcUrl: "https://base-rpc.publicnode.com",
