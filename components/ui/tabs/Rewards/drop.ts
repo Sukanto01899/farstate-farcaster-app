fe9557eee8e4e32a5040dd4ac17051ca9d26b86c
sukanto01899 <sukanto01899@gmail.com>
2026-07-04 17:39:33 +0600

    zora drop added

--- components/ui/tabs/Rewards/drop.ts
+++ components/ui/tabs/Rewards/drop.ts
@@ -129,17 +129,17 @@ export const drop = [
 
 export const questDrop = [
   {
-    id: 51,
+    id: 52,
     title: "Complete Free Box AP Bonus Quest",
     // title: "Visit Captcha Mini App",
     description:
       "Visit the Alchemy, Open free Mystery box and claim AP from quest tab.",
     // description: "Visit the Captcha, Open Captcha App and claim onchain ID.",
-    contract: abi.WCTDrop,
+    contract: abi.zoraDrop,
     chain: base,
     isActive: true,
-    icon: "https://s2.coinmarketcap.com/static/img/coins/64x64/33152.png",
-    reward: "0.5 $WCT",
+    icon: "https://s2.coinmarketcap.com/static/img/coins/64x64/35931.png",
+    reward: "2 $ZORA",
     isUpcoming: false,
     appUrl: "https://base.app/app/forgeapp.fun",
     visitLabel: "Open Alchemy",
