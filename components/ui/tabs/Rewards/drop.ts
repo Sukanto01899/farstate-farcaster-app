c634c7df67c0d33f6375b33102dae3f0eb693ea5
sukanto01899 <sukanto01899@gmail.com>
2026-09-22 17:19:52 +0600

    zora drop

--- components/ui/tabs/Rewards/drop.ts
+++ components/ui/tabs/Rewards/drop.ts
@@ -129,7 +129,7 @@ export const drop = [
 
 export const questDrop = [
   {
-    id: 57,
+    id: 58,
     title: "Complete Free Box AP Bonus Quest",
     // title: "Visit Captcha Mini App",
     description:
